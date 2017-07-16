58e0c1d0e2c8eba87ae829ed574a9d11680298bb
Chenxing Li <cx-li13@mails.tsinghua.edu.cn>
2017-07-16 14:44:10 -0400

    add performance test

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ void profile_tally(size_t wordsize_, size_t arity_, size_t csize_)
     const size_t wordsize = wordsize_;
     const size_t arity = arity_;
     const size_t csize = csize_;
-    const size_t max_layer = 0;
+    const size_t max_layer = 2;
     const bool test_serialization = false;
     const bool bit = run_r1cs_sp_ppzkpcd_tally_example<PCD_ppT>(wordsize, arity, max_layer, csize, test_serialization);
     assert(bit);
@@ -29,13 +29,7 @@ int main(int argc, char* argv[])
     start_profiling();
     PCD_pp::init_public_params();
 
-
-    size_t a[6] = {1,2,3,4,6,8};
-    size_t b[9] = {16,24,32,48,64,96,128,256,512};
-    size_t c = (size_t)(1 << ((argv[3][0]-'0') * 2 + 1));
-
-
-    profile_tally<PCD_pp>(b[argv[1][0]-'0'], a[argv[2][0]-'0'], c);
+    profile_tally<PCD_pp>(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
 
     return 0;
 }
